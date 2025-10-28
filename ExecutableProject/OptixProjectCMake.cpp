2c53beabb50a71e4ea14843d7afbc7beaccdee9c
Adriano Cicco <adriano.cicco@supsi.ch>
2025-10-28 16:18:19 +0100

    init

---
+++ ExecutableProject/OptixProjectCMake.cpp
@@ -0,0 +1,50 @@
+﻿// OptixProjectCMake.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "OptixProjectCMake.h"
+using namespace std;
+
+void initOptix()
+{
+    // -------------------------------------------------------
+    // check for available optix7 capable devices
+    // -------------------------------------------------------
+    cudaFree(0);
+    int numDevices;
+    cudaGetDeviceCount(&numDevices);
+    if (numDevices == 0)
+        throw std::runtime_error("#osc: no CUDA capable devices found!");
+    std::cout << "#osc: found " << numDevices << " CUDA devices" << std::endl;
+
+    // -------------------------------------------------------
+    // initialize optix
+    // -------------------------------------------------------
+    OPTIX_CHECK(optixInit());
+}
+
+
+/*! main entry point to this example - initially optix, print hello
+  world, then exit */
+extern "C" int main(int ac, char** av)
+{
+    try {
+        std::cout << "#osc: initializing optix..." << std::endl;
+
+        initOptix();
+
+        std::cout 
+            << "#osc: successfully initialized optix... yay!"
+            << std::endl;
+
+        // for this simple hello-world example, don't do anything else
+        // ...
+        std::cout << "#osc: done. clean exit." << std::endl;
+
+    }
+    catch (std::runtime_error& e) {
+        std::cout << "FATAL ERROR: " << e.what()
+            << std::endl;
+        exit(1);
+    }
+    return 0;
+}
---
+++ ExecutableProject/OptixProjectCMake.h
@@ -0,0 +1,23 @@
+﻿// OptixProjectCMake.h: file di inclusione per file di inclusione di sistema standard,
+// o file di inclusione specifici del progetto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Fare riferimento qui alle intestazioni aggiuntive richieste dal programma.
+
+#include <cuda_runtime.h>
+#include <optix.h>
+#include <optix_stubs.h>
+#include <optix_function_table_definition.h>
+
+#define OPTIX_CHECK( call )                                             \
+  {                                                                     \
+    OptixResult res = call;                                             \
+    if( res != OPTIX_SUCCESS )                                          \
+      {                                                                 \
+        fprintf( stderr, "Optix call (%s) failed with code %d (line %d)\n", #call, res, __LINE__ ); \
+        exit( 2 );                                                      \
+      }                                                                 \
+  }
